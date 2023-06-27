ab69c4a9603a53c049cf93afe70492572ea7322c
HaiPhham <haiphamtrung62@gmail.com>
2023-06-27 22:00:40 +0700

    fix bug

--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -164,7 +164,7 @@ function Posts({ post, socket }) {
             <Avatar
               name={post.Users.name}
               src={
-                post.Users.profile.avatarURL ||
+                post.Users.profile.avatarURL ??
                 'https://www.bing.com/images/search?view=detailV2&ccid=%2fS6CWPvq&id=401086B8664F7F68427A4E83582EA7EE515290BF&thid=OIP._S6CWPvqlA5exNtObCij-QAAAA&mediaurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.fd2e8258fbea940e5ec4db4e6c28a3f9%3frik%3dv5BSUe6nLliDTg%26riu%3dhttp%253a%252f%252fronaldmottram.co.nz%252fwp-content%252fuploads%252f2019%252f01%252fdefault-user-icon-8-300x300.jpg%26ehk%3d%252fS1gSKIqZvzeb%252fwfdAmhZtkKAmA%252fIYDlqLb8dA96Y%252fU%253d%26risl%3d%26pid%3dImgRaw%26r%3d0%26sres%3d1%26sresct%3d1%26srh%3d800%26srw%3d800&exph=300&expw=300&q=default+user&simid=608035965117479935&FORM=IRPRST&ck=D1D7FEFE7C1A04229E939E43CA1EA8C2&selectedIndex=2'
               }
             />
